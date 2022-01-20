79e78ebea0ed58ea1133a6da24aac155c230ed4c
IDevilxD <98081526+IDevilxD@users.noreply.github.com>
2022-01-20 17:59:17 +0530

    Create get.py

---
+++ plugins/get.py
@@ -0,0 +1,18 @@
+import os
+requests as r
+
+except:
+	os.system("pip install requests")
+	import requesr.get r
+link=input("Enter link: ")
+
+o = r.get(link)
+
+point(o)
+
+next=input("Want text? [y/n]: ")
+
+if next == "y":
+	print(o.text)
+else:
+	pass
