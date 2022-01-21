95b2f296677b8b67aa9f5c1b7c5fca98037ee1f1
IDevilxD <98081526+IDevilxD@users.noreply.github.com>
2022-01-21 11:28:28 +0530

    Update set.py

--- plugins/set.py
+++ plugins/set.py
@@ -1,9 +1,1 @@
-import os
-print("""
-Vars you can change:-
-1. USR [your name]
-2. COM [your machine name]""")
-var = input("Enter which var you want to change: ")
-val = input("Enter its new value: ")
-os.environ[var] = val
-print("Error: Unknown")
+
